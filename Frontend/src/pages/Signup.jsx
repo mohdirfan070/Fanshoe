8588b2a6cec6a77ab6110c3fb04c4613e93857f8
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-10-09 21:48:55 +0530

    Backend URL Integerated into Frontend

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import "./Form.css";
-import input from "@mui/material/input";
+
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
