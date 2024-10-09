29ac05674a565747e984e0471cf1f64d31b0707c
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-10-09 22:20:34 +0530

    Api not hiting

--- Backend/routes/Products.js
+++ Backend/routes/Products.js
@@ -4,7 +4,7 @@ const router = express();
 const { fetchAllProducts , fetchFav , fetchToUpdate , updateProduct } = require('../controller/Products.js');
 
 // router.route('/apiv1/products/:limit/:page').get(authUser,fetchAllProducts);
-router.route('/apiv1/products/:page').get(authUser,fetchAllProducts);
+router.route('/apiv1/products/:page').get(fetchAllProducts);
 router.route('/apiv1/productsfromfav/:id').get(authUser,fetchFav);
 router.route('/apiv1/getproducttoupdate').get(authUser,fetchToUpdate);
 router.route('/apiv1/updateproduct').patch(authUser,updateProduct);
