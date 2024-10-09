1d1128c3f8f3ddb5472030de97b7f0b3d6def0eb
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-10-09 22:04:52 +0530

    Api not hiting

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,12 +6,13 @@ export default defineConfig({
  
   server: {
         proxy: {
-            '/apiv1': {
-                target:"https://fanshoebackend.onrender.com",
-                changeOrigin: true,
-                // target: 'http://localhost:8080', //or directly yu can write 'api':'http://locolhost:8080'
-                // rewrite: (path) => path.replace(/^\/apiv1/, '')
-            }
+            "/apiv1":"https://fanshoebackend.onrender.com"
+            // '/apiv1': {
+            //     target:"https://fanshoebackend.onrender.com",
+            //     changeOrigin: true,
+            //     // target: 'http://localhost:8080', //or directly yu can write 'api':'http://locolhost:8080'
+            //     // rewrite: (path) => path.replace(/^\/apiv1/, '')
+            // }
         }
     },
     plugins: [react()],
