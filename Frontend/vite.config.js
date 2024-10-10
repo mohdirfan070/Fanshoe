bca37fc912decf1917b12d7b37a729ce41f9e1d4
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-10-10 19:41:13 +0530

    Try 1

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,19 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
  
-  server: {
-    proxy: {
-      // '/apiv1':"http://localhost:8080"
-        // "/apiv1":"https://fanshoebackend.onrender.com"
-        '/apiv1': {
-            target:"https://fanshoebackend.onrender.com",
-            changeOrigin: true,
-            // rewrite: (path) => path.replace(/^\/apiv1/, 'https://fanshoebackend.onrender.com/apiv1'),
-        //     // target: 'http://localhost:8080', //or directly yu can write 'api':'http://locolhost:8080'
-        //     // rewrite: (path) => path.replace(/^\/apiv1/, '')
-        }
-      }
-},
+//   server: {
+//     proxy: {
+//       // '/apiv1':"http://localhost:8080"
+//         // "/apiv1":"https://fanshoebackend.onrender.com"
+//         '/apiv1': {
+//             target:"https://fanshoebackend.onrender.com",
+//             changeOrigin: true,
+//             // rewrite: (path) => path.replace(/^\/apiv1/, 'https://fanshoebackend.onrender.com/apiv1'),
+//         //     // target: 'http://localhost:8080', //or directly yu can write 'api':'http://locolhost:8080'
+//         //     // rewrite: (path) => path.replace(/^\/apiv1/, '')
+//         }
+//       }
+// },
     plugins: [react()],
     build: {
         rollupOptions: {
